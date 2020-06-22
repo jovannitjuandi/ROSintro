7d036801fdf2d08bc2a05a3d0453f4caef8f11fb
jovanni tjuandi <jovanni.tjuandi@gmail.com>
2020-06-23 00:05:39 +1000

    First commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/owr01/practice/src
+# Build directory: /home/owr01/practice/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
